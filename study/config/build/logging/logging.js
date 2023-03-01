ed1d60491219077ae4d73481484f754438862bfc
softmoca <acyc0909@gmail.com>
2023-03-01 18:11:29 +0900

    'debug'

---
+++ study/config/build/logging/logging.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("helsdflo");
+//# sourceMappingURL=logging.js.map
--- study/config/src/main.ts
+++ study/config/src/main.ts
@@ -1,1 +1,12 @@
-console.log("hahaha");
+"use strict";
+class Car {
+  engine = 0;
+  move() {
+    const engine = this.engine + 1;
+    console.log("bug");
+    console.log(engine);
+  }
+}
+
+const car = new Car();
+car.move();
